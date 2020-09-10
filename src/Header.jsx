e474f85ecc97bf2daa7aca45c54b0112a642a62a
shubhambartwal <57596569+shubhambartwal@users.noreply.github.com>
2020-09-10 15:33:25 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,14 +5,15 @@ import Logo from './pictures/logo.jpeg';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import {useStateValue} from './StateProvider';
-import { auth } from 'firebase';
+import { auth } from './firebase';
 function Header() {
- const [{basket,user}]=useStateValue();
- const  login = () =>{
-   if(user){
-     auth.signOut() ;
-   }
- }; 
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
  return (
 <nav className="header">
            <Link to ="/">
@@ -26,17 +27,17 @@ function Header() {
            </div>
            <div className="header__nav">
              <Link to ={!user && "/login"} className="header__link">
-             <div  onClick={ login }  className="header__option">
+             <div  onClick={ handleAuthenticaton }  className="header__option">
              <span className="header__option1line" >Hello {user?.email} </span>
              <span className="header__option2line">{user? 'Sign Out':'Sign In'}</span>
                </div>
              </Link>
-             <Link to ="/" className="header__link">
+           
              <div className="header__option">
              <span className="header__option1line">Returns  </span>
              <span className="header__option2line">&order</span>
                </div>
-             </Link>
+             
              <Link to="/checkout" className="header__link">
                <div className="header__optionBasket">
                  <ShoppingBasketIcon />
@@ -46,7 +47,7 @@ function Header() {
            </div>
            </nav>
 
-    )
+    );
 }
 
 export default Header;
